efbb1d4e8bf097797105dc1b07c94f4de8c041f1
Rohit arya <Rohitarya56508@gmail.com>
2026-02-27 20:31:19 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,24 @@
+const hamb = document.querySelector('#header .nav-bar .nav-list .hamb');
+const mobile_menu = document.querySelector('#header .nav-bar .nav-list ul');
+const header = document.querySelector('#header .header-container');
+
+hamb.addEventListener('click', () => {
+    hamb.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+/* Custom Cursor */
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", e => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
